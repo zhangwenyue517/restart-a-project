ec6a7cd3765ce46ff4f7e7b9a74e437857f68be3
zhangwenyue <1368035616@qq.com>
2019-02-21 22:30:24 +0800

    accomplish the click industry problem

--- project-JS/homepage.js
+++ project-JS/homepage.js
@@ -1,41 +1,8 @@
-var count = 0;//定义跑动的次数
-var isgo = false;//定义动画的执行方向
-var timer;//定义计时器对象
-window.onload = function() {
-    var ul = document.getElementById("second").getElementsByTagName("ul");//获取ul元素
-    var li = ul.getElementsByTagName("li");//获取li元素
-    var btn = document.getElementById("btn");
-    var buttons = btn.getElementsByClassName("buttons");//获取所有按钮元素
-    //定义计时器，自动轮播
-    showtime();
-    function showtime() {
-        timer = setInterval(function() {
-            if(isgo == false) {
-                count++;
-                ul.style.transform = "translate("+ -1366 * count + "px)";
-                if(count >= li.length - 1) {
-                    count = li.length - 1;
-                    isgo = true;
-                }
-            }else {
-                count--;
-                ul.style.transform = "translate("+ -1366 * count + "px)";
-                if(count <= 0) {
-                    count = 0;
-                    isgo = false;
-                }
-            }
-            for(var i = 0; i < on.length; i++) {
-                buttons[i].style.backgroundColor = "#f81d1d";
-            }
-            buttons[count].style.backgroundColor = "#ff5a5a";
-        }, 4000);
-    }
-    //整个容器中鼠标悬停停止，离开恢复自动轮播
-    container.onmousemove = function() {
-        clearInterval(timer);
-    }
-    container.onmouseleave = function() {
-        showtime();
-    }
+var list1 = document.getElementById('list1');
+var list2 = document.getElementById('list2');
+function news() {
+    list2.style.display = 'inline';
+    list1.style.display = 'none';
+    list2.style.marginTop = '240px';
+    return false;
 }
