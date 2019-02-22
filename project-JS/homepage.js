fd3f314ec077d2f2171741a895048e65b9382c6b
zhangwenyue <1368035616@qq.com>
2019-02-22 22:37:10 +0800

    reorganize

--- project-JS/homepage.js
+++ project-JS/homepage.js
@@ -1,8 +1,11 @@
-var list1 = document.getElementById('list1');
-var list2 = document.getElementById('list2');
-function news() {
-    list2.style.display = 'inline';
-    list1.style.display = 'none';
-    list2.style.marginTop = '240px';
-    return false;
+window.onload = function() {
+    var list1 = document.getElementById('list1');
+    var list2 = document.getElementById('list2');
+    var industry_new = document.getElementById('industry-new');
+    //var com_new = document.getElementById('com-new');
+    industry_new.onclick = function () {
+        list2.style.display = 'inline';
+        list1.style.display = 'none';
+        list2.style.marginTop = '240px';
+    };
 }
