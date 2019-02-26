b746f6ac5269de3f3a2f7a19e49819ca872aeda5
zhangwenyue <1368035616@qq.com>
2019-02-26 17:23:29 +0800

    change some html

--- project-JS/homepage.js
+++ project-JS/homepage.js
@@ -2,10 +2,17 @@ window.onload = function() {
     var list1 = document.getElementById('list1');
     var list2 = document.getElementById('list2');
     var industry_new = document.getElementById('industry-new');
-    //var com_new = document.getElementById('com-new');
+    var com_new = document.getElementById('com-new');
     industry_new.onclick = function () {
-        list2.style.display = 'inline';
         list1.style.display = 'none';
+        list2.style.display = 'inline';
         list2.style.marginTop = '240px';
+        list1.style.color = '#000';
     };
+    com_new.onclick = function () {
+        list1.style.display = 'inline';
+        list2.style.display = 'none';
+        list2.style.marginBottom = '240px';
+        list1.style.color = '#000';
+    }
 }
