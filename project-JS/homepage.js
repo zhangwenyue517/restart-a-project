dbcaa153be5dbd996c9a6df940e5e678fe949c1a
zhangwenyue <1368035616@qq.com>
2019-02-28 22:16:27 +0800

    add the fuction of clicking on the color

--- project-JS/homepage.js
+++ project-JS/homepage.js
@@ -3,16 +3,22 @@ window.onload = function() {
     var list2 = document.getElementById('list2');
     var industry_new = document.getElementById('industry-new');
     var com_new = document.getElementById('com-new');
+    var hr_4 = document.getElementById('hr4');
+    //var isgo = true;
     industry_new.onclick = function () {
         list1.style.display = 'none';
         list2.style.display = 'inline';
         list2.style.marginTop = '240px';
-        list1.style.color = '#000';
+        this.style.color = '#000';
+        com_new.style.color = '#ADACAC';
+        //hr_4.style.right = 69 + 'px';
     };
     com_new.onclick = function () {
         list1.style.display = 'inline';
         list2.style.display = 'none';
         list2.style.marginBottom = '240px';
-        list1.style.color = '#000';
+        this.style.color = '#000';
+        industry_new.style.color = '#ADACAC';
+        //hr_4.style.right = -69 + 'px';
     }
 }
