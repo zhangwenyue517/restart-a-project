2d4382e5036675082d556d29cdfab64d1c0a60b7
zhangwenyue <1368035616@qq.com>
2019-03-06 12:25:50 +0800

    finish the part of viewpages

--- project-JS/homepage.js
+++ project-JS/homepage.js
@@ -1,17 +1,51 @@
 window.onload = function() {
-    var list1 = document.getElementById('list1');
+    var li_img = document.getElementsByClassName('li');//取到轮播图img
+    var ul = document.getElementById('ul');
+    var list1 = document.getElementById('list1');//
     var list2 = document.getElementById('list2');
     var industry_new = document.getElementById('industry-new');
     var com_new = document.getElementById('com-new');
     var hr_4 = document.getElementById('hr4');
-    //var isgo = true;
+    var isgo = true;//跑动方向
+    var count = 0;//定义跑动的次数
+    var timer;//定义计时器对象
+    var buttons = document.getElementsByClassName('buttons');
+    show();//定义计时器，自动轮播
+    function show() {
+        timer = setInterval(function() {
+            if(isgo === true) {
+                count ++;
+                ul.style.transform = "translate("+ -1349 * count +"px)";
+                if(count >= li_img.length - 1) {
+                    count = li_img.length - 1;
+                    isgo = false;
+                }
+            }else {
+                count --;
+                ul.style.transform = "translate("+ -1349 * count +"px)";
+                if(count <= 0) {
+                    count = 0;
+                    isgo = true;
+                }
+            }
+            for(var i = 0; i < buttons.length; i++) {
+                buttons[i].style.backgroundColor = "#FF5A5A";
+            }
+            buttons[count].style.backgroundColor = "#F81D1D";
+        },4000); 
+    };
+    for(i = 0; i < buttons.length; i ++) {
+        buttons[i].onmouseover = function() {
+            clearInterval(timer);
+        }
+    }
     industry_new.onclick = function () {
         list1.style.display = 'none';
         list2.style.display = 'inline';
         list2.style.marginTop = '240px';
         this.style.color = '#000';
         com_new.style.color = '#ADACAC';
-        //hr_4.style.right = 69 + 'px';
+        hr_4.style.marginLeft = '193px';
     };
     com_new.onclick = function () {
         list1.style.display = 'inline';
@@ -19,6 +53,6 @@ window.onload = function() {
         list2.style.marginBottom = '240px';
         this.style.color = '#000';
         industry_new.style.color = '#ADACAC';
-        //hr_4.style.right = -69 + 'px';
-    }
+        hr_4.style.marginLeft = '12px';
+    };
 }
