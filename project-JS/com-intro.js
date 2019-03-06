717ab17a754a1077fa7cd50313430718e10e2d40
zhangwenyue <1368035616@qq.com>
2019-03-06 18:28:09 +0800

    finish the buttons

--- project-JS/com-intro.js
+++ project-JS/com-intro.js
@@ -2,10 +2,6 @@ window.onload = function() {
     var btn1 = document.getElementById('btn1');
     var btn2 = document.getElementById('btn2');
     var btn3 = document.getElementById('btn3');
-    var img_div = document.getElementsByClassName('img-div');
-    var display = document.getElementsByClassName('display');
-    var i = img_div.length;
-    var a = display.length;
     btn1.onclick = function() {
         btn2.style.color = '#FFF';
         btn2.style.backgroundColor = 'transparent';
@@ -29,9 +25,5 @@ window.onload = function() {
         btn3.style.backgroundColor = '#FFF';
         btn2.style.color = '#FFF';
         btn2.style.backgroundColor = 'transparent';
-    }
-    if(i % 2 == 0) {
-        img_div[i].style.marginLeft = '698px';
-        display[a].style.marginRight = '718px';
     }
 }
--- project-JS/prod-center.js
+++ project-JS/prod-center.js
@@ -2,36 +2,25 @@ window.onload = function() {
     var btn1 = document.getElementById('btn1');
     var btn2 = document.getElementById('btn2');
     var btn3 = document.getElementById('btn3');
-    var img_div = document.getElementsByClassName('img-div');
-    var display = document.getElementsByClassName('display');
-    var i = img_div.length;
-    var a = display.length;
     btn1.onclick = function() {
         btn2.style.color = '#FFF';
         btn2.style.backgroundColor = 'transparent';
-        btn1.style.color = '#000';
-        btn1.style.backgroundColor = '#FFF';
+        btn1.style.backgroundColor = '#E77070';
         btn3.style.color = '#FFF';
         btn3.style.backgroundColor = 'transparent';
     }
     btn2.onclick = function() {
         btn1.style.color = '#FFF';
         btn1.style.backgroundColor = 'transparent';
-        btn2.style.color = '#000';
-        btn2.style.backgroundColor = '#FFF';
+        btn2.style.backgroundColor = '#E77070';
         btn3.style.color = '#FFF';
         btn3.style.backgroundColor = 'transparent';
     }
     btn3.onclick = function() {
         btn1.style.color = '#FFF';
         btn1.style.backgroundColor = 'transparent';
-        btn3.style.color = '#000';
-        btn3.style.backgroundColor = '#FFF';
+        btn3.style.backgroundColor = '#E77070';
         btn2.style.color = '#FFF';
         btn2.style.backgroundColor = 'transparent';
-    }
-    if(i % 2 == 0) {
-        img_div[i].style.marginLeft = '698px';
-        display[a].style.marginRight = '718px';
     }
 }
